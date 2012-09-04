570f27921ac86660186a98c7cdcbbdbc727021e0
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-09-04 20:24:36 +0400

    completely switch to Scala

--- src/swtTestApp/State.scala
+++ src/swtTestApp/State.scala
@@ -3,6 +3,8 @@ package swtTestApp
 import scala.beans.BeanProperty
 
 class State {
+  @BeanProperty var tasksOpen : Int = 2
+  @BeanProperty var tasksRunning : Int = 6
   @BeanProperty var requestsInterval : Int = 5000
 
 }
---
+++ src/swtTestApp/XwtTestApp.scala
@@ -0,0 +1,37 @@
+package swtTestApp
+
+import java.net.URL;
+
+import org.eclipse.e4.xwt.IConstants;
+import org.eclipse.e4.xwt.XWT;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+
+class XwtTestApp {
+
+}
+object XwtTestApp extends App {
+  
+  val shell =
+    XWT.load(
+      getClass.getResource( reflect.runtime.currentMirror.reflect(this).symbol.name + IConstants.XWT_EXTENSION_SUFFIX ),
+      new State
+    ).getShell
+
+  shell.layout()
+
+  { // centerInDisplay
+    val area = shell.getDisplay.getClientArea
+    shell.setBounds( area.width / 4, area.height / 4, area.width / 4, area.height / 4 )
+  }
+
+  shell.open()
+
+  while ( !shell.isDisposed ) {
+    if ( !shell.getDisplay.readAndDispatch() ) {
+      shell.getDisplay.sleep();
+    }
+  }
+
+}
