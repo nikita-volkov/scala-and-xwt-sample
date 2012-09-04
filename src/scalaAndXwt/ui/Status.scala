fc138f8cb1b1ecdfb5c652eac0bd60e17d428435
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-09-05 00:45:23 +0400

    events

--- src/scalaAndXwt/ui/Status.scala
+++ src/scalaAndXwt/ui/Status.scala
@@ -1,7 +1,15 @@
 package scalaAndXwt.ui
-import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Composite
+import org.eclipse.swt.widgets.Event
+import scalaAndXwt.state.State
+import scala.util.Random
 
 class Status( parent : Composite, style : Int )
   extends Composite( parent, style ) {
-
+  def onRefreshClick( e : Event ) {
+    State.tasksRunning = Random.nextInt( 100 )
+    State.tasksOpen = Random.nextInt( 100 )
+    println( State.tasksOpen )
+    
+  }
 }
