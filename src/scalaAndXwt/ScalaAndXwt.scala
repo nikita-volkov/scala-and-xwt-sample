16750b3ceba66c5a079084d9af6bff9d00c943fc
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-09-04 21:34:32 +0400

    state singleton

--- src/scalaAndXwt/ScalaAndXwt.scala
+++ src/scalaAndXwt/ScalaAndXwt.scala
@@ -1,26 +1,14 @@
 package scalaAndXwt
 
-import java.net.URL
-import org.eclipse.e4.xwt.IConstants
 import org.eclipse.e4.xwt.XWT
-import org.eclipse.swt.graphics.Rectangle
-import org.eclipse.swt.widgets.Control
-import org.eclipse.swt.widgets.Shell
-import reflect.runtime._
+
 import scalaAndXwt.state.State
 
 object ScalaAndXwt extends App {
 
   val shell =
-    XWT.load( getClass.getResource( "ui/MainShell.xwt" ), new State )
+    XWT.load( getClass.getResource( "ui/MainShell.xwt" ), State )
       .getShell
-  //    XWT.load(
-  //      getClass.getResource(
-  //        currentMirror.reflect( ScalaAndXwt.this ).symbol.name +
-  //          IConstants.XWT_EXTENSION_SUFFIX
-  //      ),
-  //      new State
-  //    ).getShell
 
   shell.layout()
 
--- src/scalaAndXwt/state/State.scala
+++ src/scalaAndXwt/state/State.scala
@@ -2,7 +2,7 @@ package scalaAndXwt.state
 
 import scala.beans.BeanProperty
 
-class State {
+object State {
   @BeanProperty var tasksOpen : Int = 2
   @BeanProperty var tasksRunning : Int = 6
   @BeanProperty var requestsInterval : Int = 5000
